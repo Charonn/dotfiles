d694ece71657517edf8b31836473dfd601d9c161
Marcel Hufnagel <m.hufnagel@setlog.com>
2025-03-20 10:54:01 +0100

    UPDATE

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,20 +5,31 @@ set -x _JAVA_AWT_WM_NONREPARENTING 1
 set -x AWT_TOOLKIT MToolkit
 set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
 set -x XDG_CURRENT_DESKTOP sway:dark
-set -x GTK_THEME Adwaita:dark  # Replace with your preferred dark theme
-set -x QT_STYLE_OVERRIDE Adwaita-dark  # Replace with your preferred dark theme
+set -x GTK_THEME Adwaita:dark # Replace with your preferred dark theme
+set -x QT_STYLE_OVERRIDE Adwaita-dark # Replace with your preferred dark theme
 set -x WEBKIT_DISABLE_COMPOSITING_MODE 1
 set -x MOZ_ENABLE_WAYLAND 1 firefox
+# Set GTK dark theme
+set -Ux GTK_THEME Adwaita-dark
+set -x VAULT_ADDR https://vault.setlog.lan
+# Set Qt dark mode
+set -Ux QT_QPA_PLATFORMTHEME qt5ct
 
 alias k=kubectl
 
 fish_add_path $HOME/google-cloud-sdk/bin
 fish_add_path /opt/idea-IU-243.21565.193/bin/
+fish_add_path /usr/local/go/bin
+fish_add_path $HOME/go/bin
+fish_add_path $GOPATH/bin
+fish_add_path /opt/nvim-linux-x86_64/bin
 
-if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
-    exec sway > ~/.sway.log 2>&1
+
+if test -z "$DISPLAY" -a (tty) = /dev/tty1
+    exec sway >~/.sway.log 2>&1
 end
 
+op completion fish | source
 fish_ssh_agent
 
 if status is-interactive
@@ -27,6 +38,12 @@ end
 # Check if autojump.fish exists and source it
 if test -s $HOME/.autojump/share/autojump/autojump.fish
     source $HOME/.autojump/share/autojump/autojump.fish
+end
+
+function loadenv -a file
+    while read -l line
+        eval "export $line"
+    end <$file >/dev/null
 end
 
 bind \cf 'fish -c "$HOME/.tmux/tmux-sessionizer.sh"'
--- .tmux/tmux-sessionizer.sh
+++ .tmux/tmux-sessionizer.sh
@@ -1,29 +1,36 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+  selected=$1
 else
-	selected=$(find ~/ ~/.config/ ~/workspace/ -mindepth 1 -maxdepth 4 \
-  \( -path "$HOME/workspace/jdtls-workspace*" -o -path "$HOME/jdtls-workspace*" \) -prune \
-  -o -type d -readable -print | fzf)
+  selected=$(find ~/ ~/.config/ ~/workspace/ -mindepth 1 -maxdepth 4 \
+    \( -path "$HOME/workspace/jdtls-workspace*" -o -path "$HOME/jdtls-workspace*" \) -prune \
+    -o -type d -readable -print | fzf)
 fi
 
 if [[ -z $selected ]]; then
-    exit 0
+  exit 0
 fi
+
+clear # Clears the terminal after selection
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
-    exit 0
+  tmux new-session -s "$selected_name" -c "$selected" -n "ide" -d
+  tmux send-keys -t "$selected_name:1" "nvim" C-m # Start nvim in window 1
+  tmux new-window -t "$selected_name" -n "shell" -c "$selected"
+  tmux select-window -t "$selected_name:2" # Stay on window 2 (terminal)
+  tmux attach-session -t "$selected_name"
+  exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
-else
-    tmux attach-session -t $selected_name
+if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+  tmux new-session -ds "$selected_name" -c "$selected" -n "ide"
+  tmux send-keys -t "$selected_name:1" "nvim" C-m # Start nvim in window 1
+  tmux new-window -t "$selected_name" -n "shell" -c "$selected"
 fi
 
-tmux switch-client -t $selected_name
+tmux select-window -t "$selected_name:2" # Stay on window 2 (terminal)
+tmux switch-client -t "$selected_name"
